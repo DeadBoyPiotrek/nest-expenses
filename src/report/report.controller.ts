e32c21da053e871aa7a85072f14d872bdf164035
Pio trek <pipolito2115@gmail.com>
2023-07-26 20:06:11 +0200

    feat: add crud

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -35,14 +35,35 @@ export class ReportController {
       updated_at: new Date(),
       type,
     };
+    data.report.push(newReport);
     return newReport;
   }
-  @Put()
-  updateReport() {
-    return 'report updated';
+  @Put(':id')
+  updateReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: ReportType,
+    @Param('id') id: string,
+  ) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+    if (!reportToUpdate) return 'no report found';
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      amount,
+      source,
+      updated_at: new Date(),
+    };
+    return data.report[reportIndex];
   }
-  @Delete()
-  getExpenses() {
+  @Delete(':id')
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return 'report not found';
+    data.report.splice(reportIndex, 1);
     return 'report deleted';
   }
 }
